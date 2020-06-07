6d9db46b927550a4a1d37211c8b9af9893eb427d
Jayraj Shah <jayrajcshah@gmail.com>
2020-06-07 21:56:49 +0530

    07-06-20

---
+++ coding_solutions/07-06-20/StartEnd.py
@@ -0,0 +1,5 @@
+# CODE BY JAYRAJ
+
+lst = list(map(lambda i: i*i, list(x for x in range(1, 31))))
+print('The first 5 elements of the squared list are:', lst[:5])
+print('The last 5 elements of the squared list are:', lst[-5:])
