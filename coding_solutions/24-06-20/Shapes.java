7e85beee71671731d02121bfa82236b1f0ffcbd5
Jayraj Shah <jayrajcshah@gmail.com>
2020-06-24 22:31:21 +0530

    Daily Report : 24-06-20

--- coding_solutions/24-06-20/Shapes.java
+++ coding_solutions/24-06-20/Shapes.java
@@ -1,28 +1,31 @@
-class Shape{
-  public void shape(){
+class Shape {
+  public void shape() {
     System.out.println("This is shape");
   }
 }
-class Rectangle extends Shape{
-  public void rect(){
+
+class Rectangle extends Shape {
+  public void rect() {
     System.out.println("This is rectangular shape");
   }
 }
-class Circle extends Shape{
-  public void crcl(){
+
+class Circle extends Shape {
+  public void crcl() {
     System.out.println("This is circular shape");
   }
 }
-class Square extends Rectangle{
-  public void sqr(){
+
+class Square extends Rectangle {
+  public void sqr() {
     System.out.println("Square is a rectangle");
   }
 }
 
-class Shapes{
-  public static void main(String[] args){
-    Square s = new Square(); 
-    s.shape();   
-    s.rect();   
+class Shapes {
+  public static void main(String[] args) {
+    Square s = new Square();
+    s.shape();
+    s.rect();
   }
 }
