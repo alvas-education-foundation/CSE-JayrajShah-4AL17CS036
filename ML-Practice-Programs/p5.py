ed5acc12d4a0fb0021995aa8e7d8828f87738738
Jayraj Shah <jayrajcshah@gmail.com>
2020-09-02 11:46:08 +0530

    ML Practice Programs

---
+++ ML-Practice-Programs/p5.py
@@ -0,0 +1,7 @@
+'''
+5. Write a Python program which accepts the user's first and last name and print them in reverse order with a space between them
+'''
+
+FirstName = input("Input your First Name : ")
+LastName = input("Input your Last Name : ")
+print(LastName + " " + FirstName)
